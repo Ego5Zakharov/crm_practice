6ae7ed5058f9da99a3ac5a2a64cdb2ba0b8e2091
Zaha_xk93@student.itstep.kz <Zaha_xk93@student.itstep.kz>
2024-02-11 13:56:07 +0600

    crm update

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'php1' => [
+        'php2' => [
+            '123' => 'asd'
+        ]
+    ]
+//    'name' => 'crm_practice',
+//    'host' => 'db',
+//    'port' => '3306',
+//    'username' => 'root',
+//    'password' => 'root'
+];
